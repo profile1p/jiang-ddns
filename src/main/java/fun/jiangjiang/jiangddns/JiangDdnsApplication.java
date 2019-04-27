901b7465cf3a60e36d133acc8cf4c7bdb3e56ff1
llx <llx523@gmail.com>
2019-04-27 22:58:55 +0800

    init

---
+++ src/main/java/fun/jiangjiang/jiangddns/JiangDdnsApplication.java
@@ -0,0 +1,13 @@
+package fun.jiangjiang.jiangddns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiangDdnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiangDdnsApplication.class, args);
+    }
+
+}
